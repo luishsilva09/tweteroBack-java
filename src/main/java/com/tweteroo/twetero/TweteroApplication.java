3f28949b81ba2fe2fb5e315a07c9e2b524747006
Luís Henrique <luishsilva09@gmail.com>
2023-01-19 17:48:46 -0300

    initial

---
+++ src/main/java/com/tweteroo/twetero/TweteroApplication.java
@@ -0,0 +1,13 @@
+package com.tweteroo.twetero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweteroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweteroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweteroo/twetero/TweteroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroo.twetero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweteroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
